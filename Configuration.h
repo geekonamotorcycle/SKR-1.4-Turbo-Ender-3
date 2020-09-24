b07e501ac302f48879bfe6eb1fe0af0cdae84752
A certain person <Joshua.porrata@gmail.com>
2020-09-24 19:37:20 -0400

    Add files via upload
    Adjusted Extruder steps "esteps" to 98.3 by default for my particular setup.
    
    #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 98.3 }

--- Configuration.h
+++ Configuration.h
@@ -753,7 +753,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 } /* CUSTOMVALUE */
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 98.3 } /* CUSTOMVALUE */
 
 /**
  * Default Max Feed Rate (mm/s)
